1ecdcb0c468eb78f6fb698f3f6bea589adcdb38f
github username <github email address>
2020-07-08 21:52:44 -0400

    Included saved news posts in interests and news sources methods

--- src/Reducers/newsSourcesReducer.js
+++ src/Reducers/newsSourcesReducer.js
@@ -6,6 +6,31 @@ export default (state = {
       return {
         posts: action.posts
       }
+    case 'UPDATE_NEWS_SOURCES_POSTS':
+      const updatePosts = [...state.posts]
+      if (action.post.is_news_story) {
+        return {
+          ...state,
+          posts: updatePosts.map(post => {
+            if (action.post.content === post.content && action.post.title === post.title) {
+              return action.post;
+            } else {
+              return post;
+            }
+          })
+        }
+      } else {
+        return {
+          ...state,
+          posts: updatePosts.map(post => {
+            if (post.id === action.post.id) {
+              return action.post;
+            } else {
+              return post;
+            }
+          })
+        }
+      }
     default:
       return state;
   }
